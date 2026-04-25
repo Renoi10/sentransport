4144d35bc6f2b6c036ba6023617282c693d678f1
Abdoulaye Gueye <gueyeabdoulaye727@gmail.com>
2026-04-25 17:31:51 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Statistique.css";
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <h2>10 lignes</h2>
+      <p>Réseau DDD</p>
+    </div>
+  );
+}
+
+export default Statistique;
